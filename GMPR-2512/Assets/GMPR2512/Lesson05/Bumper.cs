4b4298168273c0d4f6d1c5c74e3568c439da08d1
evelyn meetoos <eviemeetoos@gmail.com>
2026-01-29 14:26:59 -0700

    adding a bunch of code

--- GMPR-2512/Assets/GMPR2512/Lesson05/Bumper.cs
+++ GMPR-2512/Assets/GMPR2512/Lesson05/Bumper.cs
@@ -1,13 +1,35 @@
+using System;
+using NUnit.Framework;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace GMPR2512Lesson05
 {
 public class bumper : MonoBehaviour
 {
     [SerializeField] private float _bumperForce = 150f;
+    
+    [SerializeField] private Color _litColour = Color.yellow;
+
+    private bool _isLit = false;
+    private Color _originalColor;
+    private SpriteRenderer _spriteRenderer;
+
+    void Awake()
+
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+            _originalColor = _spriteRenderer.color;
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
+
+
        if (collision.collider.CompareTag("Ball"))
+       
+
+
        {
            Debug.Log($"A game object with tag {collision.collider.tag} just hit me!");
            if(collision.rigidbody != null)
